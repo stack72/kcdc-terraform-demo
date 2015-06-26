4e14aab84500fbdbdfbe915c89be0d0fe1d6ad80
James Nugent <james@jen20.com>
2015-06-26 14:53:55 -0500

    Add in demo

--- 8_private_instances.tf
+++ 8_private_instances.tf
@@ -1,68 +1,76 @@
-/*resource "aws_instance" "private_1" {*/
-  /*ami = "ami-d73818e7"*/
-  /*instance_type = "t2.micro"*/
-  /*subnet_id = "${aws_subnet.primary-public.id}"*/
-  /*security_groups = ["${aws_security_group.node.id}"]*/
-  /*availability_zone = "${aws_subnet.primary-private.availability_zone}"*/
-  /*key_name = "${aws_key_pair.kcdc_terraform.key_name}"*/
+resource "aws_instance" "private_1" {
+  ami = "ami-d73818e7"
+  instance_type = "t2.micro"
+  subnet_id = "${aws_subnet.primary-public.id}"
+  security_groups = ["${aws_security_group.node.id}"]
+  availability_zone = "${aws_subnet.primary-private.availability_zone}"
+  key_name = "${aws_key_pair.kcdc_terraform.key_name}"
 
-  /*tags {*/
-    /*Name = "private-instance-1"*/
-  /*}*/
+  tags {
+    Name = "private-instance-1"
+  }
 
-  /*connection {*/
-    /*user  = "ubuntu"*/
-    /*agent = true*/
-    /*bastion_host = "${aws_instance.nat.public_ip}"*/
-  /*}*/
+  connection {
+    user  = "ubuntu"
+    agent = true
+    bastion_host = "${aws_instance.nat.public_ip}"
+  }
 
-  /*provisioner "remote-exec" {*/
-    /*inline = "echo remote-exec works >> /tmp/remote-exec"*/
-  /*}*/
-/*}*/
+  provisioner "remote-exec" {
+    inline = "echo remote-exec works >> /tmp/remote-exec"
+  }
+}
 
-/*resource "aws_instance" "private_2" {*/
-  /*ami = "ami-d73818e7"*/
-  /*instance_type = "t2.micro"*/
-  /*subnet_id = "${aws_subnet.secondary-public.id}"*/
-  /*security_groups = ["${aws_security_group.node.id}"]*/
-  /*availability_zone = "${aws_subnet.secondary-private.availability_zone}"*/
-  /*key_name = "${aws_key_pair.kcdc_terraform.key_name}"*/
+resource "aws_instance" "private_2" {
+  ami = "ami-d73818e7"
+  instance_type = "t2.micro"
+  subnet_id = "${aws_subnet.secondary-public.id}"
+  security_groups = ["${aws_security_group.node.id}"]
+  availability_zone = "${aws_subnet.secondary-private.availability_zone}"
+  key_name = "${aws_key_pair.kcdc_terraform.key_name}"
 
-  /*tags {*/
-    /*Name = "private-instance-2"*/
-  /*}*/
+  tags {
+    Name = "private-instance-2"
+  }
 
-  /*connection {*/
-    /*user  = "ubuntu"*/
-    /*agent = true*/
-    /*bastion_host = "${aws_instance.nat.public_ip}"*/
-  /*}*/
+  connection {
+    user  = "ubuntu"
+    agent = true
+    bastion_host = "${aws_instance.nat.public_ip}"
+  }
 
-  /*provisioner "remote-exec" {*/
-    /*inline = "echo remote-exec works >> /tmp/remote-exec"*/
-  /*}*/
-/*}*/
+  provisioner "remote-exec" {
+      inline = [
+          "sudo apt-get install wget",
+          "wget 'https://www.dropbox.com/s/myg0bi3j7j8eyt1/simpledemo_1.0.0_amd64.deb'",
+          "sudo dpkg -i simpledemo_1.0.0_amd64.deb",
+          "sudo apt-get install ruby",
+          "sudo gem install pleaserun",
+          "sudo pleaserun --install -p upstart -v 1.5 --name simpledemo /usr/bin/simpledemod",
+          "sudo service simpledemo start"
+      ]
+  }
+}
 
-/*resource "aws_instance" "private_3" {*/
-  /*ami = "ami-d73818e7"*/
-  /*instance_type = "t2.micro"*/
-  /*subnet_id = "${aws_subnet.tertiary-public.id}"*/
-  /*security_groups = ["${aws_security_group.node.id}"]*/
-  /*availability_zone = "${aws_subnet.tertiary-private.availability_zone}"*/
-  /*key_name = "${aws_key_pair.kcdc_terraform.key_name}"*/
+resource "aws_instance" "private_3" {
+  ami = "ami-d73818e7"
+  instance_type = "t2.micro"
+  subnet_id = "${aws_subnet.tertiary-public.id}"
+  security_groups = ["${aws_security_group.node.id}"]
+  availability_zone = "${aws_subnet.tertiary-private.availability_zone}"
+  key_name = "${aws_key_pair.kcdc_terraform.key_name}"
 
-  /*tags {*/
-    /*Name = "private-instance-3"*/
-  /*}*/
+  tags {
+    Name = "private-instance-3"
+  }
 
-  /*connection {*/
-    /*user  = "ubuntu"*/
-    /*agent = true*/
-    /*bastion_host = "${aws_instance.nat.public_ip}"*/
-  /*}*/
+  connection {
+    user  = "ubuntu"
+    agent = true
+    bastion_host = "${aws_instance.nat.public_ip}"
+  }
 
-  /*provisioner "remote-exec" {*/
-    /*inline = "echo remote-exec works >> /tmp/remote-exec"*/
-  /*}*/
-/*}*/
+  provisioner "remote-exec" {
+    inline = "echo remote-exec works >> /tmp/remote-exec"
+  }
+}
--- 9_public_elb.tf
+++ 9_public_elb.tf
@@ -1,47 +1,47 @@
-/*resource "aws_security_group" "kcdc_elb" {*/
-  /*name = "kcdc-elb-sg"*/
-  /*description = "KCDC Elastic Load Balancer Security Group"*/
-  /*vpc_id = "${aws_vpc.default.id}"*/
+resource "aws_security_group" "kcdc_elb" {
+  name = "kcdc-elb-sg"
+  description = "KCDC Elastic Load Balancer Security Group"
+  vpc_id = "${aws_vpc.default.id}"
 
-  /*ingress {*/
-    /*from_port = 8080*/
-    /*to_port   = 8080*/
-    /*protocol  = "tcp"*/
-    /*cidr_blocks = ["0.0.0.0/0"]*/
-  /*}*/
+  ingress {
+    from_port = 8080
+    to_port   = 8080
+    protocol  = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
-  /*egress {*/
-    /*from_port = "0"*/
-    /*to_port = "0"*/
-    /*protocol = "-1"*/
-    /*cidr_blocks = ["0.0.0.0/0"]*/
-  /*}*/
+  egress {
+    from_port = "0"
+    to_port = "0"
+    protocol = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
-  /*tags {*/
-    /*Name = "KCDC Load Balancer"*/
-  /*}*/
-/*}*/
+  tags {
+    Name = "KCDC Load Balancer"
+  }
+}
 
-/*resource "aws_elb" "kcdc_elb" {*/
-  /*name = "kcdc-elb"*/
-  /*subnets = ["${aws_subnet.primary-private.id}","${aws_subnet.secondary-private.id}","${aws_subnet.tertiary-private.id}"]*/
-  /*security_groups = ["${aws_security_group.kcdc_elb.id}"]*/
-  /*cross_zone_load_balancing = true*/
-	/*internal = false*/
-  /*instances = ["${aws_instance.private_1.id}","${aws_instance.private_2.id}","${aws_instance.private_3.id}"]*/
+resource "aws_elb" "kcdc_elb" {
+  name = "kcdc-elb"
+  subnets = ["${aws_subnet.primary-private.id}","${aws_subnet.secondary-private.id}","${aws_subnet.tertiary-private.id}"]
+  security_groups = ["${aws_security_group.kcdc_elb.id}"]
+  cross_zone_load_balancing = true
+    internal = false
+  instances = ["${aws_instance.private_1.id}","${aws_instance.private_2.id}","${aws_instance.private_3.id}"]
 
-  /*listener {*/
-    /*instance_port      = 8080*/
-    /*instance_protocol  = "tcp"*/
-    /*lb_port            = 8080*/
-    /*lb_protocol        = "tcp"*/
-  /*}
+  listener {
+    instance_port      = 8080
+    instance_protocol  = "tcp"
+    lb_port            = 8080
+    lb_protocol        = "tcp"
+  }
 
-  /*health_check {*/
-    /*healthy_threshold   = 2*/
-    /*unhealthy_threshold = 2*/
-    /*interval            = 10*/
-    /*target              = "HTTP:8080"*/
-    /*timeout             = 5*/
-  /*}*/
-/*}*/
+  health_check {
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
+    interval            = 10
+    target              = "HTTP:8080"
+    timeout             = 5
+  }
+}

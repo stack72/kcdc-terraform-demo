93592365af50c65a45179a0ba7570986b7ac4c43
stack72 <public@paulstack.co.uk>
2015-06-26 13:09:04 -0500

    Adding the project structure

---
+++ 5_security_groups.tf
@@ -0,0 +1,61 @@
+resource "aws_security_group" "node" {
+  name = "kcdc-node-sg"
+  description = "Default security group that allows SSH inbound and all outbound traffic from all instances in the VPC"
+  vpc_id = "${aws_vpc.default.id}"
+
+  ingress {
+    from_port   = "22"
+    to_port     = "22"
+    protocol    = "tcp"
+    self        = true
+  }
+
+  egress {
+    from_port = "0"
+    to_port = "0"
+    protocol = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags {
+    Name = "kcdc-node-sg"
+  }
+}
+
+resource "aws_security_group" "nat" {
+  name = "kcdc-nat-sg"
+  description = "Security group for nat instances that allow SSH traffic from internet"
+  vpc_id = "${aws_vpc.default.id}"
+
+  ingress {
+    from_port = 22
+    to_port   = 22
+    protocol  = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port = 80
+    to_port   = 80
+    protocol  = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port = 443
+    to_port   = 443
+    protocol  = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port = "0"
+    to_port = "0"
+    protocol = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags {
+    Name = "kcdc-nat-sg"
+  }
+}
---
+++ terraform.tfvars
@@ -0,0 +1,3 @@
+aws_access_key="AKIAIDGHM25TIY5CD3RA"
+aws_secret_key="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"
+aws_region="us-west-2"

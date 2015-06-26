6407a526e331ca4119541051b52431efe75670ef
stack72 <public@paulstack.co.uk>
2015-06-26 13:53:38 -0500

    Adding the 3 private subnets

---
+++ 7_private_subnets.tf
@@ -0,0 +1,58 @@
+resource "aws_subnet" "primary-private" {
+  vpc_id = "${aws_vpc.default.id}"
+  cidr_block = "10.0.0.0/21"
+  availability_zone = "us-west-2a"
+  map_public_ip_on_launch = false
+  depends_on = ["aws_instance.nat"]
+  tags {
+    Name = "KCDC Private Subnet 2A"
+  }
+}
+
+resource "aws_subnet" "secondary-private" {
+  vpc_id = "${aws_vpc.default.id}"
+  cidr_block = "10.0.64.0/21"
+  availability_zone = "us-west-2b"
+  map_public_ip_on_launch = false
+  depends_on = ["aws_instance.nat"]
+  tags {
+    Name = "KCDC Private Subnet 2B"
+  }
+}
+
+resource "aws_subnet" "tertiary-private" {
+  vpc_id = "${aws_vpc.default.id}"
+  cidr_block = "10.0.128.0/21"
+  availability_zone = "us-west-2c"
+  map_public_ip_on_launch = false
+  depends_on = ["aws_instance.nat"]
+  tags {
+    Name = "KCDC Private Subnet 2C"
+  }
+}
+
+resource "aws_route_table" "private" {
+  vpc_id = "${aws_vpc.default.id}"
+  route {
+    cidr_block = "0.0.0.0/0"
+    instance_id = "${aws_instance.nat.id}"
+  }
+  tags {
+    Name = "KCDC Private Network"
+  }
+}
+
+resource "aws_route_table_association" "primary-private" {
+  subnet_id = "${aws_subnet.primary-private.id}"
+  route_table_id = "${aws_route_table.private.id}"
+}
+
+resource "aws_route_table_association" "secondary-private" {
+  subnet_id = "${aws_subnet.secondary-private.id}"
+  route_table_id = "${aws_route_table.private.id}"
+}
+
+resource "aws_route_table_association" "tertiary-private" {
+  subnet_id = "${aws_subnet.tertiary-private.id}"
+  route_table_id = "${aws_route_table.private.id}"
+}

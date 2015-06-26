7179b6bbf5352416ffc82048497862ad0877b3ec
James Nugent <james@jen20.com>
2015-06-26 13:56:06 -0500

    Comment out all the things

--- 3_public_subnets.tf
+++ 3_public_subnets.tf
@@ -1,65 +1,65 @@
-resource "aws_subnet" "primary-public" {
-  vpc_id = "${aws_vpc.default.id}"
+/*resource "aws_subnet" "primary-public" {*/
+  /*vpc_id = "${aws_vpc.default.id}"*/
 
-  cidr_block = "10.0.32.0/22"
-  availability_zone = "us-west-2a"
-  map_public_ip_on_launch = true
+  /*cidr_block = "10.0.32.0/22"*/
+  /*availability_zone = "us-west-2a"*/
+  /*map_public_ip_on_launch = true*/
 
-  tags {
-    Name = "KCDC Public Subnet 2A"
-  }
-}
+  /*tags {*/
+    /*Name = "KCDC Public Subnet 2A"*/
+  /*}*/
+/*}*/
 
-resource "aws_subnet" "secondary-public" {
-  vpc_id = "${aws_vpc.default.id}"
+/*resource "aws_subnet" "secondary-public" {*/
+  /*vpc_id = "${aws_vpc.default.id}"*/
 
-  cidr_block = "10.0.96.0/22"
-  availability_zone = "us-west-2b"
-  map_public_ip_on_launch = true
+  /*cidr_block = "10.0.96.0/22"*/
+  /*availability_zone = "us-west-2b"*/
+  /*map_public_ip_on_launch = true*/
 
-  tags {
-    Name = "KCDC Public Subnet 2B"
-  }
-}
+  /*tags {*/
+    /*Name = "KCDC Public Subnet 2B"*/
+  /*}*/
+/*}*/
 
-resource "aws_subnet" "tertiary-public" {
-  vpc_id = "${aws_vpc.default.id}"
+/*resource "aws_subnet" "tertiary-public" {*/
+  /*vpc_id = "${aws_vpc.default.id}"*/
 
-  cidr_block = "10.0.160.0/22"
-  availability_zone = "us-west-2c"
-  map_public_ip_on_launch = true
+  /*cidr_block = "10.0.160.0/22"*/
+  /*availability_zone = "us-west-2c"*/
+  /*map_public_ip_on_launch = true*/
 
-  tags {
-    Name = "KCDC Public Subnet 2C"
-  }
-}
+  /*tags {*/
+    /*Name = "KCDC Public Subnet 2C"*/
+  /*}*/
+/*}*/
 
 
-resource "aws_route_table" "public" {
-  vpc_id = "${aws_vpc.default.id}"
+/*resource "aws_route_table" "public" {*/
+  /*vpc_id = "${aws_vpc.default.id}"*/
 
-  route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = "${aws_internet_gateway.default.id}"
-  }
+  /*route {*/
+    /*cidr_block = "0.0.0.0/0"*/
+    /*gateway_id = "${aws_internet_gateway.default.id}"*/
+  /*}*/
 
-  tags {
-    Name = "KCDC Public Network"
-  }
-}
+  /*tags {*/
+    /*Name = "KCDC Public Network"*/
+  /*}*/
+/*}*/
 
-resource "aws_route_table_association" "primary-public" {
-  subnet_id = "${aws_subnet.primary-public.id}"
-  route_table_id = "${aws_route_table.public.id}"
-}
+/*resource "aws_route_table_association" "primary-public" {*/
+  /*subnet_id = "${aws_subnet.primary-public.id}"*/
+  /*route_table_id = "${aws_route_table.public.id}"*/
+/*}*/
 
-resource "aws_route_table_association" "secondary-public" {
-  subnet_id = "${aws_subnet.secondary-public.id}"
-  route_table_id = "${aws_route_table.public.id}"
-}
+/*resource "aws_route_table_association" "secondary-public" {*/
+  /*subnet_id = "${aws_subnet.secondary-public.id}"*/
+  /*route_table_id = "${aws_route_table.public.id}"*/
+/*}*/
 
-resource "aws_route_table_association" "tertiary-public" {
-  subnet_id = "${aws_subnet.tertiary-public.id}"
-  route_table_id = "${aws_route_table.public.id}"
-}
+/*resource "aws_route_table_association" "tertiary-public" {*/
+  /*subnet_id = "${aws_subnet.tertiary-public.id}"*/
+  /*route_table_id = "${aws_route_table.public.id}"*/
+/*}*/
 
